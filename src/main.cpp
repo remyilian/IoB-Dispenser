bec48e8997666f0f48cf454bcbaeb7128af1c097
itsremyilian <55468407+itsremyilian@users.noreply.github.com>
2023-06-19 20:44:29 -0400

    merge to 1 file and add more fx

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,40 @@ void setup()
   fastled_init();
 }
 
-void loop() {
+void loop()
+{
   BlynkEdgent.run();
+}
+
+void fastled_init()
+{
+  // set up ws2812, gpio2, grb order, name courtesy,12 leds
+  FastLED.addLeds<WS2812, 2, GRB>(courtesy, 12);
+  FastLED.clear();
+  FastLED.show();
+}
+
+void courtesy_set(start, stop, h, s, v)
+{
+  courtesy[start, stop] = CHSV(h, s, v);
+  FastLED.show();
+}
+
+void courtesy_fx(effect)
+{
+  switch (effect)
+  {
+  case bright:
+    courtesy_set(0, 11, 255, 255, 255);
+    break;
+  case dim:
+    courtesy_set(2, 3, 255, 255, 64);
+    courtesy_set(5, 6, 255, 255, 64);
+    break;
+    case off:
+    courtesy_set(0, 11, 0, 0, 0);
+    break;
+  default:
+    break;
+  }
 }
