98219d4c4823c1cffc791ebf1e4aa5728330a59c
Remy Rattner <remyr888@gmail.com>
2023-06-20 23:36:05 -0400

    forgot to add the fucking file

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,15 @@
-//#define BLYNK_TEMPLATE_ID "TMPL2wO-7VsYy"
-//#define BLYNK_TEMPLATE_NAME "IoB Dispenser"
-//#define BLYNK_FIRMWARE_VERSION "0.1.0"
-//#define BLYNK_PRINT Serial
-//#define USE_NODE_MCU_BOARD
-
-//#include <BlynkEdgent.h>
 #include <Arduino.h>
 #include <FastLED.h>
 #include <FlowSensor.h>
 
-//fastled
+//fastled memory
 CRGB courtesy[12];
-
-
+//flowmeter
 FlowSensor FlowMeter(YFS201, D1);
+//how much juice do you want
+int BorgQty=10;
 
+//idk the flowmeter lib says to do this with an esp
 void IRAM_ATTR count()
 {
   FlowMeter.count();
@@ -24,51 +19,53 @@ void IRAM_ATTR count()
 void setup()
 {
   
-  //BlynkEdgent.begin();
-  fastled_init();
+  // setup ws2812, gpio2, grb order, name courtesy,12 leds
+  FastLED.setMaxPowerInVoltsAndMilliamps(5,400); 
+  FastLED.addLeds<WS2812, 2, GRB>(courtesy, 12);
+  FastLED.clear();
+  FastLED.show();
+  //init flowmeter
   FlowMeter.begin(count);
+
+  //set pins
+  pinMode(D2, INPUT_PULLUP);
 }
 
 void loop()
 {
-  //BlynkEdgent.run();
+  if (digitalRead(D2) == HIGH){
+    dispense(BorgQty);
+  }
 }
 
-// void fastledinit()
-// {
-//   // set up ws2812, gpio2, grb order, name courtesy,12 leds
-//   FastLED.setMaxPowerInVoltsAndMilliamps(5,400); 
-//   FastLED.addLeds<WS2812, 2, GRB>(courtesy[12], 12);
-//   FastLED.clear();
-//   FastLED.show();
-// }
-
-void courtesyset(start, stop, h, s, v)
+void courtesy_set(int start,int stop,int h,int s,int v)
 {
-  courtesy[start, stop] = CHSV(h, s, v);
+  courtesy[start,stop] = CHSV(h,s,v);
   FastLED.show();
 }
 
-void courtesy_fx(effect)
+void courtesy_fx(int effect)
 {
   switch (effect)
   {
-  case bright:
+  case 0:
+    courtesy_set(0, 11, 0, 0, 0);
+    break;
+  case 1:
     courtesy_set(0, 11, 255, 255, 255);
     break;
-  case dim:
+  case 2:
     courtesy_set(2, 3, 255, 255, 64);
     courtesy_set(5, 6, 255, 255, 64);
     break;
-    case off:
-    courtesy_set(0, 11, 0, 0, 0);
-    break;
+
   default:
+    courtesy_set(0, 11, 0, 0, 0);
     break;
   }
 }
 
-void get_volume(){
+float get_volume(){
   		FlowMeter.read();
       return FlowMeter.getVolume();
 }
